a9732409dab48ee8a9b42b91f30aa5b46aa48dc5
teranishi-oss <teranishi@any-c.com>
2025-12-18 22:51:50 +0900

    Create menu section with customer needs-based cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
+import { Menu } from './components/Menu';
 import { Services } from './components/Services';
 import { BeforeAfter } from './components/BeforeAfter';
 import { Therapists } from './components/Therapists';
@@ -15,6 +16,7 @@ function App() {
       <Header />
       <main>
         <Hero />
+        <Menu />
         <About />
         <Services />
         <BeforeAfter />
